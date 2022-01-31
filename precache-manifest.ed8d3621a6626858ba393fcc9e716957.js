ed75276328d2af536a61676ec5af93d99092644b
Code Sandbox <hello@codesandbox.io>
2022-01-31 11:05:51 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ed8d3621a6626858ba393fcc9e716957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39962f1176cff61376f2",
+    "url": "https://fourchincookei-money.github.io/csb-1vwlx/static/css/main.e9598276.chunk.css"
+  },
+  {
+    "revision": "39962f1176cff61376f2",
+    "url": "https://fourchincookei-money.github.io/csb-1vwlx/static/js/main.a010102b.chunk.js"
+  },
+  {
+    "revision": "8b9e83a86484ee047f68",
+    "url": "https://fourchincookei-money.github.io/csb-1vwlx/static/js/runtime~main.d5c281ef.js"
+  },
+  {
+    "revision": "b9bbf43ad470f5ec5f17",
+    "url": "https://fourchincookei-money.github.io/csb-1vwlx/static/js/2.bc9e7e59.chunk.js"
+  },
+  {
+    "revision": "51d043d79ecfd038fdfb0b14404cb449",
+    "url": "https://fourchincookei-money.github.io/csb-1vwlx/index.html"
+  }
+];
